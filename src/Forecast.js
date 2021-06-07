7764eee6030fd2a9b5888339c0befc53e70c6921
marchalseverine <81558460+marchalseverine@users.noreply.github.com>
2021-06-07 15:07:06 +0200

    update design forecast

--- src/Forecast.js
+++ src/Forecast.js
@@ -1,56 +1,46 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import ForecastDay from "./ForecastDay";
-import "./Forecast.css";
-
 import axios from "axios";
 
+export default function Forecast(props) {
+  let [loaded, setLoaded] = useState(false);
+  let [forecastData, setForecastData] = useState(null);
 
-export default function Forecast (props){
+  useEffect(() => {
+    setLoaded(false);
+  }, [props.coords]);
 
-  let [loaded, setLoaded] = useState (false);
-  let [forecastData, setForecastData] = useState (null);
-
- useEffect (() =>{
-setLoaded(false);
- }, [props.coords]);
-
-  function handleResponseForecast (response){
+  function handleResponseForecast(response) {
     setForecastData(response.data.daily);
-    setLoaded (true);
-}
+    setLoaded(true);
+  }
 
+  if (loaded) {
+    return (
+      <div className="forecast-day">
+        <div className="container">
+          <div className="row d-flex justify-content-between">
+            {forecastData.map(function (dailyForecast, index) {
+              if (index < 5) {
+                return (
+                  <div className="col" key={index}>
+                    <ForecastDay forecastData={dailyForecast} />
+                  </div>
+                );
+              }
+            })}
+          </div>
+        </div>
+      </div>
+    );
+  } else {
+    let keyApi = "ef115c90a5cc57f88edb22a2c2a396c4";
+    let lat = props.coords.lat;
+    let lon = props.coords.lon;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${keyApi}&units=metric`;
 
-if (loaded){
-  return (
-    <div className="forecast-day">
-      <div className ="row">
-        {forecastData.map(function(dailyForecast, index){
+    axios.get(apiUrl).then(handleResponseForecast);
 
-if (index < 5){
-
-          return (
-            <div className="col" key ={index}>
-
-            <ForecastDay forecastData={dailyForecast} />
-             </div>
-          );
-}
-        })}
-      
-   </div>
-   </div>
-  );
-
-} else {
-
-let keyApi= "ef115c90a5cc57f88edb22a2c2a396c4";
-let lat = props.coords.lat;
-let lon = props.coords.lon;
-let apiUrl =`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${keyApi}&units=metric`
-
-axios.get(apiUrl).then(handleResponseForecast);
-   
-return (null);
-
-}
+    return null;
+  }
 }
--- src/ForecastDay.js
+++ src/ForecastDay.js
@@ -1,43 +1,32 @@
 import React from "react";
 import WeatherIcon from "./WeatherIcon";
 
+export default function ForecastDay(props) {
+  function day() {
+    let date = new Date(props.forecastData.dt * 1000);
+    let day = date.getDay();
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    return days[day];
+  }
+  function maxTemperature() {
+    let temperature = Math.round(props.forecastData.temp.max);
+    return `${temperature}º`;
+  }
 
+  function minTemperature() {
+    let temperature = Math.round(props.forecastData.temp.min);
+    return `${temperature}º`;
+  }
+  return (
+    <div className="day" id="weather-forecast-day">
+      <p>{day()}</p>
+      <WeatherIcon code={props.forecastData.weather[0].icon} size={36} />
 
-export default function ForecastDay (props){
-
-    function day() {
-        let date = new Date(props.forecastData.dt * 1000);
-        let day = date.getDay();
-        let days = [
-          "Sun",
-          "Mon",
-          "Tue",
-          "Wed",
-          "Thu",
-          "Fri",
-          "Sat"
-        ]
-          return days[day];
-        }
-   function maxTemperature(){
-       let temperature= Math.round(props.forecastData.temp.max);
-   return `${temperature}ºC`;
-    }
-
-    function minTemperature(){
-        let temperature = Math.round(props.forecastData.temp.min);
-    return `${temperature}ºC`
-    }
-    return(
-      <div className="day" id="weather-forecast-day">
-  <p>{day()}</p>
-  <WeatherIcon code={props.forecastData.weather[0].icon} size ={36} />
-   
-  
-  <br />
-  <div className="forecast-temperature">{maxTemperature()} | {minTemperature()}</div>
-</div>
-
-
-    )
+      <br />
+      <div className="forecast-temperature">
+        <span className="max-temp"> {maxTemperature()}</span> |{" "}
+        <span className="min-temp">{minTemperature()}</span>
+      </div>
+    </div>
+  );
 }

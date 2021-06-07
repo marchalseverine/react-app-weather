cc827b7b287528d64584dd532bb1473f310ed676
marchalseverine <81558460+marchalseverine@users.noreply.github.com>
2021-06-07 15:20:04 +0200

    Added space to the app

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -2,45 +2,42 @@ import React from "react";
 import WeatherIcon from "./WeatherIcon";
 import ConvertTemperature from "./ConvertTemperature";
 
-export default function WeatherInfo(props){
-    return (
-        <div className="weatherInfo">
-<div className="current-city text-center text-capitalize">
-        <h1>{props.data.city}</h1>
-        <h2>{props.data.description}</h2>
+export default function WeatherInfo(props) {
+  return (
+    <div className="weatherInfo">
+      <div className="current-city text-center text-capitalize">
+        <h1 className="current-city">{props.data.city}</h1>
+        <h2 className="description">{props.data.description}</h2>
         <WeatherIcon code={props.data.icon} />
-            </div>
-            <div className="current-description">
-      <div className="grid">
-        <div>
-          <span className="wind">
-            <p>Wind</p>
-          </span>
-          <span>
-            <h3 id="wind-speed">{props.data.wind} km/h</h3>
-          </span>
-        </div>
-        <div>
-          <span className="weather-temperature" id="temp">
-            <p>Temperature</p>
-            <h3>
-             
-              <ConvertTemperature celsius={props.data.temperature} />
-            </h3>
-          </span>
-        </div>
-        <div>
-          <span id="humidity">
-            <p>Humidity</p>
-            <span>
-              <h3 id="humidity-percent">{props.data.humidity}%</h3>
+      </div>
+      <div className="current-description">
+        <div className="grid">
+          <div>
+            <span className="wind">
+              <p>Wind</p>
             </span>
-          </span>
+            <span>
+              <h3 id="wind-speed">{props.data.wind} km/h</h3>
+            </span>
+          </div>
+          <div>
+            <span className="weather-temperature" id="temp">
+              <p>Temperature</p>
+              <h3>
+                <ConvertTemperature celsius={props.data.temperature} />
+              </h3>
+            </span>
+          </div>
+          <div>
+            <span id="humidity">
+              <p>Humidity</p>
+              <span>
+                <h3 id="humidity-percent">{props.data.humidity}%</h3>
+              </span>
+            </span>
+          </div>
         </div>
       </div>
     </div>
-</div>
-
-    );
-
+  );
 }
